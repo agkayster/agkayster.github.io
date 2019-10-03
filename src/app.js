05e967053eccc50a53e77de80c5572ecba7e181b
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-10-03 14:44:10 +0100

    home, about, navbar and scss

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
+import Navbar from './components/common/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+
+
+import 'bulma'
+import './style.scss'
 
 class App extends React.Component{
   render(){
     return(
       <main>
+        <Navbar />
         <Home />
         <About />
       </main>
