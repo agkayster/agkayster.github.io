6d8fd6977cd4c02cadd7adfd70aaef51a7bfc863
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-10-03 14:54:13 +0100

    home wor

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Home = ()=> {
   return(
-    <div>
-      <div className="Hero-body image-one">
-        <section className="Hero is-large">
-          <div className="title-container">
-            <h1 className="home title is-1">Ejike Chiboka</h1>
-            <h2 className="home subtitle is-4">A Full Stack Web Developer with an insatiable appetite for knowledge</h2>
-          </div>
-        </section>
+    <section className="Hero is-fullheight">
+      <div className="hero-body">
+        <div className="container mobile">
+          <h1 className="home title is-1">Ejike Chiboka</h1>
+          <h2 className="home subtitle is-4">A Full Stack Web Developer with an insatiable appetite for knowledge</h2>
+        </div>
       </div>
-    </div>
+    </section>
+
+
   )
 }
 
