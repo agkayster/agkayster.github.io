71409c275a8424b9393fd3846fe876bb1126f6b3
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-10-06 20:47:22 +0100

    added all logos

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -3,34 +3,100 @@ import React from 'react'
 class Skills extends React.Component{
   render(){
     return(
-      <section className = "section">
-        <div id="skills" className= "container">
-          <div className="columns is-multiline">
-            <div className="column is-full-desktop is-full-tablet">
-              <h2 className="title is-2">skills & experience.</h2>
-              <ul className="tech-list is-multiline">
-                <li >
-                  <img src="./images/react_logo.svg" />
-                  <span className="tech-logos">React</span>
-                </li>
-                <li >
-                  <img src="./images/html_5_logo.svg" />
-                  <span className="tech-logos">HTML5</span>
-                </li>
-                <li >
-                  <img src="./images/python_logo.svg" />
-                  <span className="tech-logos">PYTHON</span>
-                </li>
-                <li >
-                  <img src="./images/bulma_logo.svg" />
-                  <span className="tech-logos">BULMA</span>
-                </li>
-              </ul>
+      <div id="Techskills">
+        <section className = "section">
+          <div id="skills" className= "container">
+            <div className="columns is-multiline">
+              <div className="column is-half-desktop is-full-tablet">
+                <h2 className="title is-2 is-half-desktop">skills & experience.</h2>
+                <ul className="tech-list is-multiline">
+                  <li >
+                    <img src="./images/react_logo.svg" />
+                    <span className="tech-logos">React</span>
+                  </li>
+                  <li >
+                    <img src="./images/html_5_logo.svg" />
+                    <span className="tech-logos">HTML5</span>
+                  </li>
+                  <li >
+                    <img src="./images/python_logo.svg" />
+                    <span className="tech-logos">Python 3</span>
+                  </li>
+                  <li >
+                    <img src="./images/bulma_logo.svg" />
+                    <span className="tech-logos">Bulma</span>
+                  </li>
+                  <li >
+                    <img src="./images/css_3_logo.svg" />
+                    <span className="tech-logos">CSS3</span>
+                  </li>
+                  <li >
+                    <img src="./images/JavaScript_logo.svg" />
+                    <span className="tech-logos">JavaScript</span>
+                  </li>
+                  <li >
+                    <img src="./images/git_logo.svg" />
+                    <span className="tech-logos">Git</span>
+                  </li>
+                  <li >
+                    <img src="./images/github_logo.svg" />
+                    <span className="tech-logos">GitHub</span>
+                  </li>
+                  <li >
+                    <img src="./images/heroku_logo.svg" />
+                    <span className="tech-logos">Heroku</span>
+                  </li>
+                  <li >
+                    <img src="./images/express_logo.svg" />
+                    <span className="tech-logos">Express</span>
+                  </li>
+                  <li >
+                    <img src="./images/sass_logo.svg" />
+                    <span className="tech-logos">Sass</span>
+                  </li>
+                  <li >
+                    <img src="./images/nodejs_logo.svg" />
+                    <span className="tech-logos">Node.js</span>
+                  </li>
+                  <li >
+                    <img src="./images/atom_io_logo.svg" />
+                    <span className="tech-logos">Atom</span>
+                  </li>
+                  <li >
+                    <img src="./images/mongoddb_logo.svg" />
+                    <span className="tech-logos">Mongoddb</span>
+                  </li>
+                  <li >
+                    <img src="./images/npm_logo.svg" />
+                    <span className="tech-logos">npm</span>
+                  </li>
+                  <li >
+                    <img src="./images/postgresql_logo.svg" />
+                    <span className="tech-logos">PostgreSQL</span>
+                  </li>
+                  <li >
+                    <img src="./images/slack_logo.svg" />
+                    <span className="tech-logos">Slack</span>
+                  </li>
+                  <li >
+                    <img src="./images/trello_logo.svg" />
+                    <span className="tech-logos">Trello</span>
+                  </li>
+                  <li >
+                    <img src="./images/webpack_logo.svg" />
+                    <span className="tech-logos">Webpack</span>
+                  </li>
+                  <li >
+                    <img src="./images/yarn_logo.svg" />
+                    <span className="tech-logos">Yarn</span>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
 
-      </section>
+        </section>
+      </div>
     )
   }
 }
