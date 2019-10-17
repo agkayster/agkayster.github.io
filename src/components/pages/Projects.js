5af311516224e0bf4421d33e45cc43d8edd0ffdf
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-10-17 10:18:06 +0100

    changed tech

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,7 +11,7 @@ class Projects extends React.Component{
         {
           id: '1',
           title: 'Space Invaders',
-          tech: 'Python | Flask | PostgreSQL | React | Webpack | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | Heroku',
+          tech: 'JavaScript (ES6) | HTML5 | CSS framework | SCSS',
           description: 'Space Invaders is an entertaining browser-based arcade style game. Challenge your reaction skills by stopping Aliens from invading earth while avoiding getting killed by the same Aliens.It was an individual project built in a week, and was both the first proper game I had built, and my first real-world type practice with JavaScript.',
           type: 'JavaScript ES6',
           image: './images/Space Invaders.png',
@@ -21,7 +21,7 @@ class Projects extends React.Component{
         {
           id: '2',
           title: 'NewsAPI',
-          tech: 'Python | Flask | PostgreSQL | React | Webpack | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | Heroku',
+          tech: 'React | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | External API',
           description: 'NEWS API is a news aggregator collating online news from different online papers across various countries. It was a pair coding project built in 2 days, and was both the first aggregating site we built, and our first real-world type practice with REACT.',
           type: 'React App',
           image: './images/NewsAPI.png',
@@ -31,7 +31,7 @@ class Projects extends React.Component{
         {
           id: '3',
           title: 'Lost the Plot' ,
-          tech: 'Python | Flask | PostgreSQL | React | Webpack | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | Heroku',
+          tech: 'Express | NodeJS | React | Webpack | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | Heroku| MongoDB| Mongoose',
           description: 'Lost the plot is a web-site primarily aimed at Londoners who are looking for a plot of land on which to grow their own food (or to help grow food, or take part in other gardening activities).This project has been built as part of a learning module in General Assembly"s Software Engineering Immersive Course using JavaScript and React on the front-end and Express and MongoDB on the back-end.',
           type: 'Full-Stack',
           image: './images/Lost the Plot.png',
@@ -41,7 +41,7 @@ class Projects extends React.Component{
         {
           id: '4',
           title: 'Gympro',
-          tech: 'Python | Flask | PostgreSQL | React | Webpack | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | Heroku',
+          tech: 'Python | Django | PostgreSQL | React | Webpack | Ajax | JavaScript (ES6) | HTML5 | Bulma (CSS framework) | SCSS | Heroku| Babel',
           description: 'Gympro is a web application primarily aimed at those who love to go to the gym and work out. It allows users: 1. Search for exercises for each category of the body e.g. Thighs, Legs, Shoulders, Chest, Core and Arms. 2. Create workout programmes like Summer Body,etc.',
           type: 'Full Stack',
           image: './images/GymproApp.png',
@@ -63,7 +63,7 @@ class Projects extends React.Component{
             <img className="aboutlogo" src="https://imgur.com/07QH95m.png"/>
 
             <h2 className="black-title is-full-tablet projectstitle">projects.</h2>
-            
+
             <div className="columns is-multiline">
               {this.state.data.map(projectCard =>
                 <div key={projectCard.id} className={classes}>
