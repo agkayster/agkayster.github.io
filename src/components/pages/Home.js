213c48a9c6c0dd2d4196f7c3bfef8de5b44a7ee5
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-10-23 20:51:44 +0100

    revert

---
+++ src/components/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Home = ()=> {
+  return(
+    <div id="home">
+      <section className="section">
+        <div className="hero-body">
+          <div className="container mobile">
+            <div className="devdesc">
+              <h1 className="home title is-1">Ejike Chiboka.</h1>
+              <h2 className="home subtitle is-4">A Full Stack Web Developer with an insatiable appetite for knowledge.</h2>
+            </div>
+
+            <section id="section07" className="demo">
+              <a href="#about"><span></span><span></span><span></span></a>
+            </section>
+          </div>
+        </div>
+      </section>
+    </div>
+
+
+  )
+}
+
+export default Home
