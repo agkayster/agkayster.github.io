201a409a1590021d825f909dab4b49965c511f92
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-11-12 21:46:14 +0100

    rollback from gh

---
+++ src/components/pages/Skills.js
@@ -0,0 +1,157 @@
+import React from 'react'
+
+class Skills extends React.Component{
+  render(){
+    return(
+      <div id="skills">
+        <section className="section skills">
+
+          <div className="container">
+
+            <div className="columns is-multiline">
+
+              <div className="column is-full-desktop is-full-tablet">
+                <img className="aboutlogo" src="https://imgur.com/07QH95m.png"/>
+
+                <h2 className="black-title is-half-desktop is-multiline is-full-tablet">skills & experience.</h2>
+
+                <ul className="tech-list is-multiline">
+                  <li >
+                    <img src="./images/react_logo.svg" />
+                    <span className="tech-logos">React</span>
+                  </li>
+                  <li >
+                    <img src="./images/html_5_logo.svg" />
+                    <span className="tech-logos">HTML5</span>
+                  </li>
+                  <li >
+                    <img src="./images/python_logo.svg" />
+                    <span className="tech-logos">Python 3</span>
+                  </li>
+                  <li >
+                    <img src="./images/bulma_logo.svg" />
+                    <span className="tech-logos">Bulma</span>
+                  </li>
+                  <li >
+                    <img src="./images/css_3_logo.svg" />
+                    <span className="tech-logos">CSS3</span>
+                  </li>
+                  <li >
+                    <img src="./images/JavaScript_logo.svg" />
+                    <span className="tech-logos">JavaScript</span>
+                  </li>
+                  <li >
+                    <img src="./images/git_logo.svg" />
+                    <span className="tech-logos">Git</span>
+                  </li>
+                  <li >
+                    <img src="./images/github_logo.svg" />
+                    <span className="tech-logos">GitHub</span>
+                  </li>
+                  <li >
+                    <img src="./images/heroku_logo.svg" />
+                    <span className="tech-logos">Heroku</span>
+                  </li>
+                  <li >
+                    <img src="./images/express_logo.svg" />
+                    <span className="tech-logos">Express</span>
+                  </li>
+                  <li >
+                    <img src="./images/sass_logo.svg" />
+                    <span className="tech-logos">Sass</span>
+                  </li>
+                  <li >
+                    <img src="./images/nodejs_logo.svg" />
+                    <span className="tech-logos">Node.js</span>
+                  </li>
+                  <li >
+                    <img src="./images/atom_io_logo.svg" />
+                    <span className="tech-logos">Atom</span>
+                  </li>
+                  <li >
+                    <img src="./images/mongoddb_logo.svg" />
+                    <span className="tech-logos">Mongoddb</span>
+                  </li>
+                  <li >
+                    <img src="./images/npm_logo.svg" />
+                    <span className="tech-logos">npm</span>
+                  </li>
+                  <li >
+                    <img src="./images/postgresql_logo.svg" />
+                    <span className="tech-logos">PostgreSQL</span>
+                  </li>
+                  <li >
+                    <img src="./images/slack_logo.svg" />
+                    <span className="tech-logos">Slack</span>
+                  </li>
+                  <li >
+                    <img src="./images/trello_logo.svg" />
+                    <span className="tech-logos">Trello</span>
+                  </li>
+                  <li >
+                    <img src="./images/webpack_logo.svg" />
+                    <span className="tech-logos">Webpack</span>
+                  </li>
+                  <li >
+                    <img src="./images/yarn_logo.svg" />
+                    <span className="tech-logos">Yarn</span>
+                  </li>
+                  <li >
+                    <img src="./images/django_logo.svg" />
+                    <span className="tech-logos">Django</span>
+                  </li>
+                  <li >
+                    <img src="./images/babel_logo.svg" />
+                    <span className="tech-logos">Babel</span>
+                  </li>
+                  <li >
+                    <img src="./images/mysql_logo.svg" />
+                    <span className="tech-logos">MySQL</span>
+                  </li>
+                  <li >
+                    <img src="./images/mocha_logo.svg" />
+                    <span className="tech-logos">Mocha</span>
+                  </li>
+                </ul>
+              </div>
+              <div className="exp">
+                <div className="expga">
+                  <p>GENERAL ASSEMBLY SOFTWARE ENGINEERING IMMERSIVE, LONDON, JUNE till  SEPTEMBER 2019:<br />
+                  This was a 12 weeks Software Engineering Immersive course that was focused on learning and developing the following software skills which are: HTML, CSS, GIT, JavaScript(ES6), Node.js, Express, React.js, Public and Restful API’s, Python, Django, MongoDB (NoSQL5), PostgreSQL, CRUD API and DOM manipulation, this was a combination of both Frontend and Backend.<br />
+                    <div className="skillsparagraph">
+                   We were also made to build four separate web applications using Full Stack applications as part of our four different projects, two of those projects were group projects while the remaining two were solo projects.
+                    </div>
+                  </p>
+                </div>
+                <div className="expbfe">
+                  <p>BLUEFLAME ENGINEERING LTD, OPERATIONS DIRECTOR, LAGOS, NIGERIA JULY 2013 till JUNE 2019:<br />
+                    As a Director of Operations of a startup, my job was to ensure that all projects that were running for which we were deploying IT software solutions for our clients, were completed within the designed completion period.<br />
+                    <div className="skillsparagraph">
+                    Ensure that revenue being generated was tracked.<br />
+                    </div>
+                    <div className="skillsparagraph">
+                    Reduction in organizational expenditure by applying cost-cutting measures while acquiring relevant resources for organizational growth.<br />
+                    </div>
+                  </p>
+                </div>
+                <div className="expglo">
+                  <p>GLOBACOM NIGERIA LTD, PROJECT MANAGER FEBRUARY, LAGOS, 2011-2013
+                  </p>
+                </div>
+                <div className="expericsson">
+                  <p>ERICSSON MANAGED SERVICES, BSS HIGH-LEVEL SUPPORT SPECIALIST, LAGOS, AUGUST 2009-2011
+                  </p>
+                </div>
+                <div className="expzain">
+                  <p>ZAIN NIGERIA LTD, HIGH LEVEL SUPPORT SPECIALIST, LAGOS, MARCH 2006 - 2009
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+export default Skills
