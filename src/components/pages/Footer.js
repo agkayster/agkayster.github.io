b7a08be5a479594a3c8e3ef9d8ce482d2e27f331
agkayster <ejike.chiboka@gmail.com>
2020-02-20 16:56:53 +0100

    took down hat logo

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,24 +1,48 @@
 import React from 'react'
 
-class Footer extends React.Component{
-  render(){
-    return(
+class Footer extends React.Component {
+  render() {
+    return (
       <footer>
         <div id="contact" className="contactcontainer">
-          <img className="aboutlogo" src="https://imgur.com/07QH95m.png"/>
+          <img className="aboutlogo" src="https://i.imgur.com/lvYJz7K.jpg" />
           <h2 className="title is-2 contact">contact.</h2>
 
-
-          <a href="mailto:ejike.chiboka@gmail.com" target="_blank" rel="noopener noreferrer"><i className="far fa-envelope"></i></a>
-          <a href="https://github.com/agkayster" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-          <a href="https://www.linkedin.com/in/ejike-chiboka-pmp/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-          <a href="https://www.twitter.com/agkayster" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+          <a
+            href="mailto:ejike.chiboka@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="far fa-envelope"></i>
+          </a>
+          <a
+            href="https://github.com/agkayster"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ejike-chiboka-pmp/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a
+            href="https://www.twitter.com/agkayster"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
           <div>
-            <span><small>Developed by Ejike Chiboka | © 2019</small></span>
+            <span>
+              <small>Developed by Ejike Chiboka | © 2019</small>
+            </span>
           </div>
         </div>
       </footer>
-
     )
   }
 }
--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,114 +1,116 @@
 import React from 'react'
 
-class Skills extends React.Component{
-  render(){
-    return(
+class Skills extends React.Component {
+  render() {
+    return (
       <div id="skills">
         <section className="section skills">
-
           <div className="container">
-
             <div className="columns is-multiline">
-
               <div className="column is-full-desktop is-full-tablet">
-                <img className="aboutlogo" src="https://imgur.com/07QH95m.png"/>
+                <img
+                  className="aboutlogo"
+                  src="https://i.imgur.com/lvYJz7K.jpg"
+                />
 
-                <h2 className="black-title is-half-desktop is-multiline is-full-tablet">skills & experience.</h2>
+                <h2 className="black-title is-half-desktop is-multiline is-full-tablet">
+                  skills & experience.
+                </h2>
 
                 <ul className="tech-list is-multiline">
-                  <li >
+                  <li>
                     <img src="./images/react_logo.svg" />
                     <span className="tech-logos">React</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/html_5_logo.svg" />
                     <span className="tech-logos">HTML5</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/python_logo.svg" />
                     <span className="tech-logos">Python 3</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/bulma_logo.svg" />
                     <span className="tech-logos">Bulma</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/css_3_logo.svg" />
                     <span className="tech-logos">CSS3</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/javascript_logo.svg" />
                     <span className="tech-logos">JavaScript</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/git_logo.svg" />
                     <span className="tech-logos">Git</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/github_logo.svg" />
                     <span className="tech-logos">GitHub</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/heroku_logo.svg" />
                     <span className="tech-logos">Heroku</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/express_logo.svg" />
                     <span className="tech-logos">Express</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/sass_logo.svg" />
                     <span className="tech-logos">Sass</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/nodejs_logo.svg" />
                     <span className="tech-logos">Node.js</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/atom_io_logo.svg" />
                     <span className="tech-logos">Atom</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/mongoddb_logo.svg" />
                     <span className="tech-logos">Mongoddb</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/npm_logo.svg" />
                     <span className="tech-logos">npm</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/postgresql_logo.svg" />
                     <span className="tech-logos">PostgreSQL</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/slack_logo.svg" />
                     <span className="tech-logos">Slack</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/trello_logo.svg" />
                     <span className="tech-logos">Trello</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/webpack_logo.svg" />
                     <span className="tech-logos">Webpack</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/yarn_logo.svg" />
                     <span className="tech-logos">Yarn</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/django_logo.svg" />
                     <span className="tech-logos">Django</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/babel_logo.svg" />
                     <span className="tech-logos">Babel</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/mysql_logo.svg" />
                     <span className="tech-logos">MySQL</span>
                   </li>
-                  <li >
+                  <li>
                     <img src="./images/mocha_logo.svg" />
                     <span className="tech-logos">Mocha</span>
                   </li>
@@ -116,34 +118,64 @@ class Skills extends React.Component{
               </div>
               <div className="exp">
                 <div className="expga">
-                  <p>GENERAL ASSEMBLY SOFTWARE ENGINEERING IMMERSIVE, LONDON, JUNE till  SEPTEMBER 2019:<br />
-                  This was a 12 weeks Software Engineering Immersive course that was focused on learning and developing the following software skills which are: HTML, CSS, GIT, JavaScript(ES6), Node.js, Express, React.js, Public and Restful API’s, Python, Django, MongoDB (NoSQL5), PostgreSQL, CRUD API and DOM manipulation, this was a combination of both Frontend and Backend.<br />
+                  <p>
+                    GENERAL ASSEMBLY SOFTWARE ENGINEERING IMMERSIVE, LONDON,
+                    JUNE till SEPTEMBER 2019:
+                    <br />
+                    This was a 12 weeks Software Engineering Immersive course
+                    that was focused on learning and developing the following
+                    software skills which are: HTML, CSS, GIT, JavaScript(ES6),
+                    Node.js, Express, React.js, Public and Restful API’s,
+                    Python, Django, MongoDB (NoSQL5), PostgreSQL, CRUD API and
+                    DOM manipulation, this was a combination of both Frontend
+                    and Backend.
+                    <br />
                     <div className="skillsparagraph">
-                   During the course I built four separate web applications using Full Stack applications as part of our four different projects, two of those projects were group projects while the remaining two were solo projects.
+                      During the course I built four separate web applications
+                      using Full Stack applications as part of our four
+                      different projects, two of those projects were group
+                      projects while the remaining two were solo projects.
                     </div>
                   </p>
                 </div>
                 <div className="expbfe">
-                  <p>BLUEFLAME ENGINEERING LTD, OPERATIONS DIRECTOR, LAGOS, NIGERIA JULY 2013 till JUNE 2019:<br />
-                    As a Director of Operations of a startup, my job was to ensure that all projects that were running for which we were deploying IT software solutions for our clients, were completed within the designed completion period.<br />
+                  <p>
+                    BLUEFLAME ENGINEERING LTD, OPERATIONS DIRECTOR, LAGOS,
+                    NIGERIA JULY 2013 till JUNE 2019:
+                    <br />
+                    As a Director of Operations of a startup, my job was to
+                    ensure that all projects that were running for which we were
+                    deploying IT software solutions for our clients, were
+                    completed within the designed completion period.
+                    <br />
                     <div className="skillsparagraph">
-                    Ensure that revenue being generated was tracked.<br />
+                      Ensure that revenue being generated was tracked.
+                      <br />
                     </div>
                     <div className="skillsparagraph">
-                    Reduction in organizational expenditure by applying cost-cutting measures while acquiring relevant resources for organizational growth.<br />
+                      Reduction in organizational expenditure by applying
+                      cost-cutting measures while acquiring relevant resources
+                      for organizational growth.
+                      <br />
                     </div>
                   </p>
                 </div>
                 <div className="expglo">
-                  <p>GLOBACOM NIGERIA LTD, PROJECT MANAGER FEBRUARY, LAGOS, 2011-2013
+                  <p>
+                    GLOBACOM NIGERIA LTD, PROJECT MANAGER FEBRUARY, LAGOS,
+                    2011-2013
                   </p>
                 </div>
                 <div className="expericsson">
-                  <p>ERICSSON MANAGED SERVICES, BSS HIGH-LEVEL SUPPORT SPECIALIST, LAGOS, AUGUST 2009-2011
+                  <p>
+                    ERICSSON MANAGED SERVICES, BSS HIGH-LEVEL SUPPORT
+                    SPECIALIST, LAGOS, AUGUST 2009-2011
                   </p>
                 </div>
                 <div className="expzain">
-                  <p>ZAIN NIGERIA LTD, HIGH LEVEL SUPPORT SPECIALIST, LAGOS, MARCH 2006 - 2009
+                  <p>
+                    ZAIN NIGERIA LTD, HIGH LEVEL SUPPORT SPECIALIST, LAGOS,
+                    MARCH 2006 - 2009
                   </p>
                 </div>
               </div>
